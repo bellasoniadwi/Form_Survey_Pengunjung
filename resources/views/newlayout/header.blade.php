7faab0f56721f03068b6b99480a6590136ba6491
auryno19 <aurynoaga19@gmail.com>
2023-07-04 13:07:04 +0700

    header

--- resources/views/newlayout/header.blade.php
+++ resources/views/newlayout/header.blade.php
@@ -1,30 +1,40 @@
 <!-- header section start -->
-<div class="header_section">
+<div class="header_section sticky-top">
     <div class="container">
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand"href="index.html"><img src="{{ asset('style/images/logo.png')}}"></a>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+         <a class="navbar-brand" href="index.html"><img id="logo" width="200px" height="75px" src="{{ asset('style/images/logo.png')}}"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
              <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                   <a class="nav-link" href="index.html">Home</a>
+                   <a class="nav-link" href="index.html">Berand</a>
                 </li>
                 <li class="nav-item">
-                   <a class="nav-link" href="about.html">About</a>
+                   <a class="nav-link" href="about.html">Tentang</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Produk
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
                 </li>
                 <li class="nav-item">
-                   <a class="nav-link" href="services.html">Services</a>
+                   <a class="nav-link" href="gallery.html">Pengalaman</a>
                 </li>
                 <li class="nav-item">
-                   <a class="nav-link" href="gallery.html">Vehicles</a>
+                   <a class="nav-link" href="client.html">Galeri</a>
                 </li>
                 <li class="nav-item">
-                   <a class="nav-link" href="client.html">Client</a>
+                   <a class="nav-link" href="contact.html">Berita</a>
                 </li>
                 <li class="nav-item">
-                   <a class="nav-link" href="contact.html">Contact</a>
+                   <a class="nav-link" href="contact.html">Hubungi Kami</a>
                 </li>
              </ul>
              <form class="form-inline my-2 my-lg-0">
@@ -33,13 +43,18 @@
        </nav>
     </div>
  </div>
+{{-- 
+ <script>
+   window.onscroll = function() {scrollFunction()};
+   
+   function scrollFunction() {
+     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+       document.getElementById("logo").width = "150px";
+       document.getElementById("logo").heigth = "60px";
+     } else {
+      document.getElementById("logo").width = "200px";
+       document.getElementById("logo").heigth = "75px";
+     }
+   }
+   </script> --}}
  <!-- header section end -->
- <div class="call_text_main">
-    <div class="container">
-       <div class="call_taital">
-          <div class="call_text"><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span class="padding_left_15">Location</span></a></div>
-          <div class="call_text"><a href="#"><i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left_15">(+71) 8522369417</span></a></div>
-          <div class="call_text"><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i><span class="padding_left_15">demo@gmail.com</span></a></div>
-       </div>
-    </div>
- </div>
--- resources/views/newlayout/main.blade.php
+++ resources/views/newlayout/main.blade.php
@@ -16,6 +16,8 @@
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
       <!-- bootstrap css -->
       <link rel="stylesheet" type="text/css" href="{{ asset('style/css/bootstrap.min.css')}}">
       <!-- style css -->
@@ -32,10 +34,11 @@
       <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
    </head>
    <body>
+
     @include('sweetalert::alert')
-    
+
     @include('newlayout.header')
-    
+
         @yield('content')
 
     @include('newlayout.footer')
