713b99f5cb6879536c3415cf4cfac926e4931eed
bellasoniadwi <bellasoniadwi@gmail.com>
2023-07-04 12:45:45 +0700

    create survey

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Survey;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Http\Request;
+
+class SurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('survey', ['title' => 'Survey']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('survey');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request -> validate([
+            'nama' => 'required',
+            'instansi' => 'required',
+            'no_hp' => 'required|max:13',
+            'saran' => 'required'
+        ]);
+
+        $survey = new Survey();
+        $survey->nama = $request->get('nama');
+        $survey->instansi = $request->get('instansi');
+        $survey->no_hp = $request->get('no_hp');
+        $survey->saran = $request->get('saran');
+        $survey->save();
+        
+        Alert::success('Masukan anda berhasil direkam');
+        return redirect()->route('survey.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
